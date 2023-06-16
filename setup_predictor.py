a64c696ca91d6d1f1de98b44699fe260c324efbc
Quoc-Huy Nguyen <quochuyn@uci.edu>
2023-06-16 13:47:59 -0700

    update pipeline architecture

--- setup_predictor.py
+++ setup_predictor.py
@@ -1,4 +1,4 @@
-# train_draft_position_predictor.py
+# setup_predictor.py
 
 import json
 import pickle
@@ -13,7 +13,6 @@ from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -23,6 +22,11 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransfo
 from sentence_transformers import SentenceTransformer
 
 
+
+# errors from incorrect formatting of input to TfidfVectorizer was resolved with
+# https://stackoverflow.com/questions/26367075/countvectorizer-attributeerror-numpy-ndarray-object-has-no-attribute-lower
+def _get_text_data(x):
+    return x.ravel()
 
 class CustomBertTransformer(BaseEstimator, TransformerMixin):
 
@@ -40,37 +44,27 @@ class CustomBertTransformer(BaseEstimator, TransformerMixin):
         # return bert embeddings
         embeddings = self.model.encode(X)
         return embeddings
-        
 
-# errors from incorrect formatting of input to TfidfVectorizer was resolved with
-# https://stackoverflow.com/questions/26367075/countvectorizer-attributeerror-numpy-ndarray-object-has-no-attribute-lower
-def _get_text_data(x):
-    return x.ravel()
-
-def train(prospect_df, numeric_cols=None, categorical_cols=None, text_cols=None):
+def setup(numeric_cols=None, categorical_cols=None, text_cols=None, func=None):
     r"""
-    Train and test a model to predict the draft position. The idea to organize
-    the pipeline into preprocessing the features based on their data type comes
-    from the course SIADS 696.
+    Setup a model for prediction. The idea to organize the pipeline into preprocessing 
+    the features based on their data type comes from the course SIADS 696.
 
     Parameters
     ----------
-    prospect_df : pandas.DataFrame
-        The NHL prospects dataframe ready to be vectorized and
-        trained on the model.
     numeric_cols : list, default=None
         The numeric columns/features of X.
     categorical_cols : list, default=None
         The categorical columns/features of X, but not including the text columns.
     text_cols : list, default=None
         The text columns/features of X.
-    
+    func : sklearn.estimator
+        The classification/regression algorithm from scikit-learn.
+        
     Returns
     -------
     model :
-        The trained scikit-learn model.
-    data : dict
-        A dictionary holding the train and test data for the model.
+        The untrained scikit-learn model.
     """
 
     if numeric_cols is None:
@@ -79,13 +73,6 @@ def train(prospect_df, numeric_cols=None, categorical_cols=None, text_cols=None)
         categorical_cols = ['Position']
     if text_cols is None:
         text_cols = ['all_reports']
-
-    X = prospect_df[numeric_cols + categorical_cols + text_cols]
-    y = prospect_df['Drafted']
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=42
-    )
 
     numeric_transformer = Pipeline(
         steps=[
@@ -124,56 +111,8 @@ def train(prospect_df, numeric_cols=None, categorical_cols=None, text_cols=None)
     model = Pipeline(
         steps=[
             ('features', feature_transformer),
-            ('clf', LinearRegression())
+            ('regressor', func())
         ]
     )
 
-    model.fit(X_train, y_train)
-
-    # best found for now to be 0.16 
-    # without draft position (I think there is bias with that data)
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
-    metrics = {
-        **model.best_params_,
-        'train_score' : model.score(X_train, y_train),
-        'test_score' : model.score(X_test, y_test),
-        'mae' : mean_absolute_error(y_test, y_test_pred),
-        'mse' : mean_squared_error(y_test, y_test_pred),
-        'r2' : r2_score(y_test, y_test_pred),
-    }
-
-    return model, metrics
-
-
-
-if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'input_file', help='the preprocessed NHL prospects data file (.CSV)'
-    )
-    parser.add_argument(
-        'output_model', help='the trained model (.PKL)'
-    )
-    parser.add_argument(
-        'output_metrics', help='the metrics (.JSON)',
-    )
-    parser.add_argument(
-        '-v', '--verbose', action='store_true',
-        help='display metrics',
-    )
-    args = parser.parse_args()
-
-    preprocessed_df = pd.read_csv(args.input_file)
-    model, metrics = train(preprocessed_df)
-
-    if args.verbose:
-        print(json.dumps(metrics, indent=4))
-
-    with open(args.output_model, 'wb+') as write_file:
-        pickle.dump(model, write_file)
-
-    with open(args.output_metrics, 'a+') as write_file:
-        write_file.write(json.dumps(metrics, indent=4) + '\n')
+    return model
